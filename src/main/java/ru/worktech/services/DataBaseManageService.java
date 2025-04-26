5ebbcaad4dacdbba1c4d13a3b2dd175f271f9782
BugDone26 <bkachanovskiy@gmail.com>
2025-04-26 20:47:54 +0300

    JDBC работает, добавилена зависимость для работы jdbc.

--- src/main/java/ru/worktech/services/DataBaseManageService.java
+++ src/main/java/ru/worktech/services/DataBaseManageService.java
@@ -3,18 +3,17 @@ package ru.worktech.services;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataBaseManageService {
 
-    private static final String URL = "jdbc:mysql://91.211.249.37:32500/wt_backend_test"; // Замените на ваш URL
-    private static final String USER = "backend_test"; // Замените на ваше имя пользователя
-    private static final String PASSWORD = "d2343&^2dsjsds"; // Замените на ваш пароль
+    private static final String URL = "jdbc:mysql://91.211.249.37:32500/wt_backend_test";
+    private static final String USER = "backend_test";
+    private static final String PASSWORD = "d2343&^2dsjsds";
 
     // Метод для удаления пользователя по email
     public void deleteUser(String email) {
-        String sql = "DELETE FROM users WHERE email = ?"; // Замените 'users' на название вашей таблицы
+        String sql = "DELETE FROM users WHERE email = ?";
 
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
@@ -31,23 +30,4 @@ public class DataBaseManageService {
             e.printStackTrace();
         }
     }
-
-    public int getUserEmail(String email) {
-        String sql = "SELECT email FROM users WHERE username = ?"; // Замените 'username' на ваше поле
-
-        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-
-            preparedStatement.setString(1, email);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                return resultSet.getInt("id");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return -1; // Возвращаем -1, если пользователь не найден
-    }
 }
-
