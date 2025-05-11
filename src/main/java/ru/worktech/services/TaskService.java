ae80d562df6351840333a9b828f6b4ff02fabaf6
BugDone26 <bkachanovskiy@gmail.com>
2025-05-11 16:31:52 +0300

    Классы для управления БД перенесены в отдельную папку, создан метод для удаления задач после их тестового создания и изменения

--- src/main/java/ru/worktech/services/TaskService.java
+++ src/main/java/ru/worktech/services/TaskService.java
@@ -4,6 +4,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import ru.worktech.core.BaseApiService;
 import ru.worktech.models.CreateTaskRequest;
+import ru.worktech.models.UpdateTaskRequest;
 
 import static ru.worktech.endpoints.Endpoints.CREATE_TASK_ENDPOINT;
 import static ru.worktech.endpoints.Endpoints.EDITE_TASK_ENDPOINT;
@@ -14,11 +15,17 @@ public class TaskService extends BaseApiService {
         return getSpec(request).post(CREATE_TASK_ENDPOINT);
     }
 
-    public Response editTask(CreateTaskRequest request) {
+    public Response editTask(UpdateTaskRequest request) {
         return getSpec(request).put(EDITE_TASK_ENDPOINT);
     }
 
     private RequestSpecification getSpec(CreateTaskRequest request) {
+        return getRequestSpec()
+                .body(request)
+                .when();
+    }
+
+    private RequestSpecification getSpec(UpdateTaskRequest request) {
         return getRequestSpec()
                 .body(request)
                 .when();
