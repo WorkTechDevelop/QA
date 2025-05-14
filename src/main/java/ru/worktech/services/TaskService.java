05a9fc0a1d68bdcf2cf9000df8b0d793b761fead
ChaykinDenis <chaichkin.denis@gmail.com>
2025-05-14 09:06:59 +0300

    написал тесты на создание задачи и превел названия к общепринятому формату

--- src/main/java/ru/worktech/services/TaskService.java
+++ src/main/java/ru/worktech/services/TaskService.java
@@ -11,12 +11,15 @@ import static ru.worktech.endpoints.Endpoints.EDITE_TASK_ENDPOINT;
 public class TaskService extends BaseApiService {
 
     public Response createTask(CreateTaskRequest request) {
-        return getSpec(request).post(CREATE_TASK_ENDPOINT);
+        return getSpec(request)
+                .post(CREATE_TASK_ENDPOINT);
     }
 
     public Response editTask(CreateTaskRequest request) {
-        return getSpec(request).put(EDITE_TASK_ENDPOINT);
+        return getSpec(request)
+                .put(EDITE_TASK_ENDPOINT);
     }
+
 
     private RequestSpecification getSpec(CreateTaskRequest request) {
         return getRequestSpec()
--- src/test/java/ru/worktech/registration_test/CreateTaskTests.java
+++ src/test/java/ru/worktech/registration_test/CreateTaskTests.java
@@ -1,9 +1,10 @@
 package ru.worktech.registration_test;
 
 import org.testng.annotations.Test;
+import ru.worktech.core.BaseApiService;
 import ru.worktech.steps.TaskSteps;
 
-import static org.apache.http.HttpStatus.SC_CREATED;
+import static org.apache.http.HttpStatus.*;
 import static ru.worktech.models.CreateTaskRequest.CreateTaskRequestBuilder;
 import static ru.worktech.models.CreateTaskRequest.builder;
 
@@ -11,19 +12,147 @@ public class CreateTaskTests {
 
     private final TaskSteps taskSteps = new TaskSteps();
 
-    @Test
-    public void successfulCreateTask() {
-    taskSteps.createTask(getDefaultCreateTask()
-                    .build())
-            .checkStatusCode(SC_CREATED);
+    @Test(testName = "TK-32-1-Создание новой задачи")
+    public void testCreateTaskSuccess() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .build())
+                .checkStatusCode(SC_CREATED);
     }
 
+    @Test(testName = "TK-32-2-Создание задачи с минимальной длиной TITLE (1 символ)")
+    public void testCreateTaskSuccessWithMinTitleLength() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .title("T")
+                        .build())
+                .checkStatusCode(SC_CREATED);
+    }
 
+    @Test(testName = "TK-32-3-Создание задачи с максимальной длиной TITLE (255 символов)")
+    public void testCreateTaskSuccessWithMaxTitleLength() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .title("T".repeat(255))
+                        .build())
+                .checkStatusCode(SC_CREATED);
+    }
 
+    @Test(testName = "TK-32-4-Cоздание задачи с максимальной длиной DESCRIPTION (4096 символов)")
+    public void testCreateTaskSuccessWithMaxDescriptionLength() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .description("C".repeat(4096))
+                        .build())
+                .checkStatusCode(SC_CREATED);
+    }
 
+    @Test(testName = "TK-32-5-Cоздание задачи без необязательных полей (DESCRIPTION, SPRINT_ID, ESTIMATION)")
+    public void testCreateTaskSuccessWithoutOptionalFields() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .description(null)
+                        .sprintId(null)
+                        .estimation(null)
+                        .build())
+                .checkStatusCode(SC_CREATED);
+    }
 
+    @Test(testName = "TK-32-6-Создание задачи без обязательного поля (TITLE)")
+    public void testCreateTaskFailWithoutTitle() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .title(null)
+                        .build())
+                .checkStatusCode(SC_BAD_REQUEST);
+    }
 
+    @Test(testName = "TK-32-7-Создание задачи без обязательного поля (PRIORITY)")
+    public void testCreateTaskFailWithoutPriority() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .priority(null)
+                        .build())
+                .checkStatusCode(SC_BAD_REQUEST);
+    }
 
+    @Test(testName = "TK-32-8-Создание задачи с некоректным значением (PRIORITY)")
+    public void testCreateTaskFailWithInvalidPriority() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .priority("INVALID")
+                        .build())
+                .checkStatusCode(SC_BAD_REQUEST);
+    }
+
+    @Test(testName = "TK-32-9-Создание задачи без обязательного поля (ASSIGNEE)")
+    public void testCreateTaskFailWithoutAssignee() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .assignee(null)
+                        .build())
+                .checkStatusCode(SC_BAD_REQUEST);
+    }
+
+    @Test(testName = "TK-32-10-Создание задачи с некоректным значением (ASSIGNEE)")
+    public void testCreateTaskFailWithInvalidAssignee() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .assignee("INVALID")
+                        .build())
+                .checkStatusCode(SC_BAD_REQUEST);
+    }
+
+    @Test(testName = "TK-32-11-Создание задачи без обязательного поля (PROJECT_ID)")
+    public void testCreateTaskFailWithoutProjectId() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .projectId(null)
+                        .build())
+                .checkStatusCode(SC_BAD_REQUEST);
+    }
+
+    @Test(testName = "TK-32-12-Создание задачи с некоректным значением (PROJECT_ID)")
+    public void testCreateTaskFailWithInvalidProjectId() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .projectId("INVALID")
+                        .build())
+                .checkStatusCode(SC_BAD_REQUEST);
+    }
+
+    @Test(testName = "TK-32-13-Создание задачи без обязательного поля (TASK_TYPE)")
+    public void testCreateTaskFailWithoutTaskType() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .taskType(null)
+                        .build())
+                .checkStatusCode(SC_BAD_REQUEST);
+    }
+
+    @Test(testName = "TK-32-14-Создание задачи с некоректным значением (TASK_TYPE)")
+    public void testCreateTaskFailWithInvalidTaskType() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .taskType("INVALID")
+                        .build())
+                .checkStatusCode(SC_BAD_REQUEST);
+    }
+
+    @Test(testName = "TK-32-15-Создание задачи с отрицательным значением (ESTIMATION)")
+    public void testCreateTaskFailWithNegativeEstimation() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .estimation("-1")
+                        .build())
+                .checkStatusCode(SC_BAD_REQUEST);
+    }
+
+    @Test(testName = "TK-32-16-Создание задачи с некоректным значением поля (SPRINT_ID)")
+    public void testCreateTaskFailWithInvalidSprintId() {
+        taskSteps.createTask(getDefaultCreateTask()
+                        .sprintId("INVALID")
+                        .build())
+                .checkStatusCode(SC_BAD_REQUEST);
+    }
+
+    @Test(testName = "TK-32-17-Создание задачи неавторизованным пользователем")
+    public void testCreateTaskFailUnauthenticated() {
+        BaseApiService.setIgnoreAuth(true);
+        taskSteps.createTask(getDefaultCreateTask()
+                        .build())
+                .checkStatusCode(SC_UNAUTHORIZED);
+        BaseApiService.setIgnoreAuth(false);
+    }
+
+    @Test(testName = "TK-32-18-Создание задачи с повторяющимися ключами json")
+    public void createTaskWithDuplicateJsonKeys() {
+    }
 
     private CreateTaskRequestBuilder getDefaultCreateTask() {
         return builder()
