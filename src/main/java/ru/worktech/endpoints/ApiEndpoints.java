de5f5c02af1a94a60ae9852793e61632c96ad995
BugDone26 <bkachanovskiy@gmail.com>
2025-05-26 10:12:44 +0300

    Написан тимплейт для readme.md

--- src/main/java/ru/worktech/endpoints/ApiEndpoints.java
+++ src/main/java/ru/worktech/endpoints/ApiEndpoints.java
@@ -11,28 +11,12 @@ public enum ApiEndpoints {
     AUTHORIZATION_ENDPOINT("/work-task/v1/login"),
     CREATE_TASK_ENDPOINT("/work-task/v1/task/create-task"),
     EDITE_TASK_ENDPOINT("/work-task/v1/task/update-task"),
-    GET_TASK_BY_ID_ENDPOINT("/work-task/v1/task/{{taskId}}"),
-    GET_TASK_IN_PROJECT_ENDPOINT("/work-task/v1/task/tasks-in-project?project-id-929"),
-    GET_TASK_BY_ID_PROJECT_ENDPOINT("/work-task/v1/task/project-tasks/project-id-929");
+    GET_TASK_BY_ID_ENDPOINT("/work-task/v1/task/{{taskId}}");
 
     private final String template;
 
     ApiEndpoints(String template) {
         this.template = template;
-    }
-
-    public String resolve(String... args) {
-        String url = template.replace("{{taskId}}", "%s");
-        return args.length == 0
-                ? url
-                : format(url, (Object[]) args);
-    }
-
-    public String resolveProjectEndpoint(String... args) {
-        String url = template.replace("{{taskId}}", "%s");
-        return args.length == 0
-                ? url
-                : format(url, (Object[]) args);
     }
 
     public String getAddress() {
