ff7176f7234b4a337dc25df57d2710f25313f2b2
ChaykinDenis <chaichkin.denis@gmail.com>
2025-06-04 13:29:03 +0300

    Актуализировал ручки регистрации и авторизации

--- src/main/java/ru/worktech/endpoints/ApiEndpoints.java
+++ src/main/java/ru/worktech/endpoints/ApiEndpoints.java
@@ -1,9 +1,6 @@
 package ru.worktech.endpoints;
 
 import lombok.Getter;
-import ru.worktech.models.UpdateTaskStatusRequest;
-
-import static java.lang.String.format;
 
 @Getter
 public enum ApiEndpoints {
@@ -12,20 +9,12 @@ public enum ApiEndpoints {
     AUTHORIZATION_ENDPOINT("/work-task/v1/auth/login"),
     CREATE_TASK_ENDPOINT("/work-task/v1/task/create-task"),
     EDITE_TASK_ENDPOINT("/work-task/v1/task/update-task"),
-    GET_TASK_BY_ID_ENDPOINT("/work-task/v1/task/{{taskId}}"),
     UPDATE_TASK_STATUS("/work-task/v1/task/update-status");
 
     private final String template;
 
     ApiEndpoints(String template) {
         this.template = template;
-    }
-
-    public String resolve(String... args) {
-        String url = template.replace("{{taskId}}", "%s");
-        return args.length == 0
-                ? url
-                : format(url, (Object[]) args);
     }
 
     public String getAddress() {
