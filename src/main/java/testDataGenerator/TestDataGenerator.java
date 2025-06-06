a27bef8aa07160491f29a00daebb0d7d9bf69ea7
ChaykinDenis <chaichkin.denis@gmail.com>
2025-06-06 14:43:47 +0300

    совместил классы EmailGenerator.java и TaskTitleGenerator.java в общий класс TestDataGenerator

--- src/main/java/testDataGenerator/TestDataGenerator.java
+++ src/main/java/testDataGenerator/TestDataGenerator.java
@@ -1,9 +1,9 @@
 package testDataGenerator;
 
-import static java.lang.System.*;
+import static java.lang.System.currentTimeMillis;
 import static java.util.UUID.randomUUID;
 
-public class EmailGenerator {
+public class TestDataGenerator {
 
     public static String generateRandomEmail() {
         return "testUser" + currentTimeMillis() + "@example.com";
@@ -11,5 +11,9 @@ public class EmailGenerator {
 
     public static String generateRandomPassword() {
         return randomUUID().toString().substring(0, 10);
+    }
+
+    public static String generateTaskTitle() {
+        return "Test" + currentTimeMillis() + "Task";
     }
 }
