0526620bbdbad150e9985f49758397c8d0e6fe97
ChaykinDenis <chaichkin.denis@gmail.com>
2025-06-06 12:06:31 +0300

    Переиминовал тест успешной авторизации под общий стиль, сделал параметризацию всех негативных тестов, вынес методы: generateRandomEmail() и generateRandomPassword() в класс testDataGenerator

--- src/test/java/ru/worktech/registration_test/AuthorizationTests.java
+++ src/test/java/ru/worktech/registration_test/AuthorizationTests.java
@@ -1,90 +1,45 @@
 package ru.worktech.registration_test;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.worktech.steps.UserSteps;
 
-import static java.util.UUID.randomUUID;
-import static org.apache.http.HttpStatus.*;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.apache.http.HttpStatus.SC_UNAUTHORIZED;
 import static ru.worktech.models.request.AuthorizationRequest.AuthorizationRequestBuilder;
 import static ru.worktech.models.request.AuthorizationRequest.builder;
+import static testDataGenerator.testDataGenerator.generateRandomEmail;
+import static testDataGenerator.testDataGenerator.generateRandomPassword;
 
 public class AuthorizationTests {
 
     private final UserSteps userSteps = new UserSteps();
 
-
     @Test(testName = "TK-311-1-Успешная авторизация")
-    public void testSuccessfulAuthorization() {
+    public void testAuthorizationSuccess() {
         userSteps.loginUser(getDefaultAuthorization().build())
-                .checkStatusCode(SC_OK);
+                .assertStatus(SC_OK);
     }
 
-    @Test(testName = "TK-311-2-Авторизация без Email")
-    public void testAuthorizationWithoutEmail() {
-        userSteps.loginUser(
-                        builder()
-                                .username(null)
-                                .password("defaultPassword")
-                                .build())
-                .checkStatusCode(SC_UNAUTHORIZED);
+    @DataProvider(name = "dataProvider")
+    public Object[][] dataProvider() {
+        return new Object[][]{
+                {null, "defaultPassword"},
+                {"default@gmail.com", null},
+                {null, null},
+                {"default.gmail.com", "defaultPassword"},
+                {" default.gmail.com ", "defaultPassword"},
+                {generateRandomEmail(), generateRandomPassword()},
+        };
     }
 
-    @Test(testName = "TK-311-3-Авторизация без пароля")
-    public void testAuthorizationWithoutPassword() {
-        userSteps.loginUser(
-                        builder()
-                                .username("default@gmail.com")
-                                .password(null)
-                                .build())
-                .checkStatusCode(SC_UNAUTHORIZED);
-    }
-
-    @Test(testName = "TK-311-4-Авторизация без Email и пароля")
-    public void testAuthorizationWithEmptyData() {
-        userSteps.loginUser(
-                        builder()
-                                .username(null)
-                                .password(null)
-                                .build())
-                .checkStatusCode(SC_UNAUTHORIZED);
-    }
-
-    @Test(testName = "TK-311-6-Авторизация с некоректным Email (без @)")
-    public void testAuthorizationWithIncorrectEmail() {
-        userSteps.loginUser(
-                        builder()
-                                .username("default.gmail.com")
-                                .password("defaultPassword")
-                                .build())
-                .checkStatusCode(SC_UNAUTHORIZED);
-    }
-
-    @Test(testName = "TK-311-7-Авторизация c пробелами перед и после Email")
-    public void testAuthorizationWithSpacesBeforeAndAfterEmail() {
-        userSteps.loginUser(
-                        builder()
-                                .username(" default@gmail.com ")
-                                .password("defaultPassword")
-                                .build())
-                .checkStatusCode(SC_UNAUTHORIZED);
-    }
-
-    @Test(testName = "TK-311-8-Авторизация незаригестрированного пользователя")
-    public void testAuthorizationNotExistUser() {
-        userSteps.loginUser(
-                        builder()
-                                .username(generateRandomEmail())
-                                .password(generateRandomPassword())
-                                .build())
-                .checkStatusCode(SC_UNAUTHORIZED);
-    }
-
-    private String generateRandomEmail() {
-        return "user" + randomUUID() + "@mail.com";
-    }
-
-    private String generateRandomPassword() {
-        return randomUUID().toString().substring(0, 10);
+    @Test(testName = "ТК-311-Тесты на авторизацию с невалидными данными", dataProvider = "dataProvider")
+    public void testAuthorizationFail(String email, String password) {
+        userSteps.loginUser(builder()
+                        .username(email)
+                        .password(password)
+                        .build())
+                .assertStatus(SC_UNAUTHORIZED);
     }
 
     private AuthorizationRequestBuilder getDefaultAuthorization() {
