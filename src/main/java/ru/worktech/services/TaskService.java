8dff3af36774da430eea810b01922e20761cda05
ChaykinDenis <chaichkin.denis@gmail.com>
2025-06-30 13:43:37 +0300

    создал степ создания задачи через мапу для параметризации

--- src/main/java/ru/worktech/services/TaskService.java
+++ src/main/java/ru/worktech/services/TaskService.java
@@ -1,28 +1,35 @@
 package ru.worktech.services;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import ru.worktech.core.BaseApiService;
-import ru.worktech.models.UpdateTaskStatusRequest;
-import ru.worktech.models.request.CreateTaskRequest;
-import ru.worktech.models.request.UpdateTaskRequest;
+import ru.worktech.models.TaskDto;
+import ru.worktech.models.request.UpdateTaskStatusRequest;
 
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 import static ru.worktech.endpoints.ApiEndpoints.*;
 
 public class TaskService extends BaseApiService {
 
-    public Response createTask(CreateTaskRequest request) {
+    public Response createTask(TaskDto request) {
         return getSpec(request)
                 .post(CREATE_TASK_ENDPOINT.getAddress());
     }
 
-    public Response createTaskWithoutAuth(CreateTaskRequest request) {
-        return getSpec(request)
-                .header("Authorization", "")
+    public Response createTaskMap(Map<String, Object> taskMap) {
+        return getSpec(taskMap)
                 .post(CREATE_TASK_ENDPOINT.getAddress());
     }
 
-    public Response editTask(UpdateTaskRequest request) {
+    public Response createTaskWithoutAuth(TaskDto request) {
+        return getSpecWithOutAuth(request)
+                .post(CREATE_TASK_ENDPOINT.getAddress());
+    }
+
+    public Response editTask(TaskDto request) {
         return getSpec(request).put(EDITE_TASK_ENDPOINT.getAddress());
     }
 
@@ -31,14 +38,18 @@ public class TaskService extends BaseApiService {
     }
 
     public Response updateTaskStatusWithOutAuth(UpdateTaskStatusRequest request) {
-        return getSpec(request)
-                .header("Authorization", "")
+        return getSpecWithOutAuth(request)
                 .post(UPDATE_TASK_STATUS.getAddress());
-
     }
 
     private RequestSpecification getSpec(Object request) {
         return getRequestSpec()
+                .body(request)
+                .when();
+    }
+
+    private RequestSpecification getSpecWithOutAuth(Object request) {
+        return getRequestSpecWithOutAuth()
                 .body(request)
                 .when();
     }
--- src/main/java/ru/worktech/steps/TaskSteps.java
+++ src/main/java/ru/worktech/steps/TaskSteps.java
@@ -3,32 +3,37 @@ package ru.worktech.steps;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import ru.worktech.core.AssertableResponse;
-import ru.worktech.models.UpdateTaskStatusRequest;
+import ru.worktech.models.TaskDto;
+import ru.worktech.models.request.UpdateTaskStatusRequest;
 import ru.worktech.models.request.CreateTaskRequest;
-import ru.worktech.models.request.CreateTaskRequest.CreateTaskRequestBuilder;
-import ru.worktech.models.request.UpdateTaskRequest;
 import ru.worktech.services.TaskService;
 
-import static ru.worktech.models.request.CreateTaskRequest.*;
+import java.util.Map;
 
 public class TaskSteps {
 
     private final TaskService taskService = new TaskService();
 
     @Step("Создать задачу")
-    public AssertableResponse createTask(CreateTaskRequest request) {
+    public AssertableResponse createTask(TaskDto request) {
         Response response = taskService.createTask(request);
         return new AssertableResponse(response);
     }
 
+    @Step("Создать задачу")
+    public AssertableResponse createTaskMap(Map<String, Object> taskMap) {
+        Response response = taskService.createTaskMap(taskMap);
+        return new AssertableResponse(response);
+    }
+
     @Step("Создать задачу без авторизации")
-    public AssertableResponse createTaskWithOutAuth(CreateTaskRequest request) {
+    public AssertableResponse createTaskWithOutAuth(TaskDto request) {
         Response response = taskService.createTaskWithoutAuth(request);
         return new AssertableResponse(response);
     }
 
     @Step("Обновить задачу")
-    public AssertableResponse updateTask(UpdateTaskRequest request) {
+    public AssertableResponse updateTask(TaskDto request) {
         Response response = taskService.editTask(request);
         return new AssertableResponse(response);
     }
@@ -45,15 +50,5 @@ public class TaskSteps {
         return new AssertableResponse(response);
     }
 
-    public static CreateTaskRequestBuilder getDefaultCreateTask() {
-        return builder()
-                .title("TestEntity")
-                .description("Correct")
-                .assignee("a4069488-7d8f-40bc-80e1-025322316901")
-                .priority("HIGH")
-                .projectId("project-123")
-                .sprintId("0001")
-                .taskType("BUG")
-                .estimation(5);
-    }
+
 }
