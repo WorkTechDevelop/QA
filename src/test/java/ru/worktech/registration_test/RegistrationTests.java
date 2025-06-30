fa3fbdffc7d90b3bb798463699771ec2fba00735
ChaykinDenis <chaichkin.denis@gmail.com>
2025-06-30 13:39:13 +0300

    добавил параметризированные тесты

--- src/test/java/ru/worktech/registration_test/RegistrationTests.java
+++ src/test/java/ru/worktech/registration_test/RegistrationTests.java
@@ -2,17 +2,15 @@ package ru.worktech.registration_test;
 
 import DataBaseManageServices.query.DeleteUserFromDataBase;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.worktech.models.request.RegistrationRequest;
 import ru.worktech.steps.UserSteps;
 
-import java.util.Objects;
-
+import static java.util.Objects.nonNull;
 import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.apache.http.HttpStatus.SC_OK;
-import static ru.worktech.models.request.RegistrationRequest.RegistrationRequestBuilder;
-import static ru.worktech.models.request.RegistrationRequest.builder;
-import static testDataGenerator.TestDataGenerator.generateRandomEmail;
+import static testDataGenerator.TestDataGenerator.*;
 
 public class RegistrationTests {
 
@@ -22,7 +20,7 @@ public class RegistrationTests {
 
     @AfterMethod
     public void afterMethod() {
-        if (Objects.nonNull(userEmail)) {
+        if (nonNull(userEmail)) {
             dbManage.deleteUserByEmail(userEmail);
         }
     }
@@ -30,85 +28,69 @@ public class RegistrationTests {
     @Test(testName = "TK-311-1-Успешная регистрации нового пользователя")
     public void testRegistrationSuccessRegistration() {
         userEmail = generateRandomEmail();
-        userSteps.registerUser(getDefaultRegistration().email(userEmail).build())
-                .assertStatus(SC_OK);
-    }
-
-    @Test(testName = "TK-311-2-Проверка регистрации пользователя, который уже существует")
-    public void testRegistrationFailExistedUser() {
-        userSteps.registerUser(getDefaultRegistration().build());
-        userSteps.registerUser(getDefaultRegistration().build())
-                .assertStatus(SC_BAD_REQUEST);
-    }
-
-    @Test(testName = "TK-311-?-")
-    public void testRegistrationFailOnEmptyEmail() {
-        userSteps.registerUser(getDefaultRegistration().email("").build())
-                .assertStatus(SC_BAD_REQUEST);
-    }
-
-    @Test(testName = "TK-311-3-Проверка email с пробелами")
-    public void testRegistrationFailUserWithSpaces() {
-        userSteps.registerUser(getDefaultRegistration().email(" default@gmail.com ").build())
-                .assertStatus(SC_BAD_REQUEST);
-    }
-
-    @Test(testName = "TK-311-4-Проверка минимальной длины пароля.")
-    public void testRegistrationFailShortPassword() {
-        userSteps.registerUser(getDefaultRegistration().password("Av1234").confirmPassword("Av1234").build())
-                .assertStatus(SC_BAD_REQUEST);
-    }
-
-    @Test(testName = "TK-311-5-Проверка регистрации пользователя с несовпадающими паролями при подтверждении")
-    public void testRegistrationFailUserWithMismatchedPasswords() {
-        userSteps.registerUser(getDefaultRegistration().password("defaultPassword123")
-                .confirmPassword("defaultPassword12").build()).assertStatus(SC_BAD_REQUEST);
-    }
-
-    @Test(testName = "TK-311-6-Проверка Email без \"@\"")
-    public void testRegistrationFailIncorrectEmail() {
-        userSteps.registerUser(getDefaultRegistration().email("default.gmail.com").build())
-                .assertStatus(SC_BAD_REQUEST);
-    }
-
-    @Test(testName = "TK-311-7-Проверка email без доменной части")
-    public void testRegistrationFailWithoutDomain() {
-        userSteps.registerUser(getDefaultRegistration().email("default@ru").build())
-                .assertStatus(SC_BAD_REQUEST);
-    }
-
-    @Test(testName = "TK-311-8-Проверка email с недопустимыми символами")
-    public void testRegistrationFailSpecialSymbol() {
-        userSteps.registerUser(getDefaultRegistration().email("testdEmail*mail.ru").build())
-                .assertStatus(SC_BAD_REQUEST);
-    }
-
-    @Test(testName = "ТК-311-10-Поле middleName не является обязательным")
-    public void testRegistrationMiddleNameNotRequired() {
-        userEmail = generateRandomEmail();
-        RegistrationRequest request = getDefaultRegistration()
-                .lastName("defaultname")
-                .firstName("defaultFirstName")
-                .email(userEmail)
-                .password("StrongPassword123!")
-                .confirmPassword("StrongPassword123!")
-                .middleName(null)
-                .build();
+        var request = getDefaultRegistration()
+                .setEmail(generateRandomEmail());
 
         userSteps.registerUser(request)
                 .assertStatus(SC_OK);
     }
 
-    private RegistrationRequestBuilder getDefaultRegistration() {
-        return builder()
-                .email("default@.com")
-                .password("defaultPassword")
-                .confirmPassword("defaultPassword")
-                .lastName("defaultLastName")
-                .firstName("defaultFirstName")
-                .middleName("defaultMiddleName")
-                .phone("+79991001010")
-                .birthDate("01-01-1971")
-                .gender("MALE");
+    @Test(testName = "ТК-311-10-Поле middleName не является обязательным")
+    public void testRegistrationMiddleNameNotRequired() {
+        userEmail = generateRandomEmail();
+        var request = getDefaultRegistration()
+                .setEmail(userEmail)
+                .setMiddleName("");
+
+        userSteps.registerUser(request)
+                .assertStatus(SC_OK);
+    }
+
+    @Test(testName = "TK-311-2-Проверка регистрации пользователя, который уже существует")
+    public void testRegistrationFailExistedUser() {
+        userSteps.registerUser(getDefaultRegistration());
+
+        userSteps.registerUser(getDefaultRegistration())
+                .assertStatus(SC_BAD_REQUEST);
+    }
+
+    @Test(testName = "TK-311-4-Проверка минимальной длины пароля.")
+    public void testRegistrationFailShortPassword() {
+        var request = getDefaultRegistration()
+                .setPassword("Av1234")
+                .setConfirmPassword("Av1234");
+
+        userSteps.registerUser(request)
+                .assertStatus(SC_BAD_REQUEST);
+    }
+
+    @Test(testName = "TK-311-5-Проверка регистрации пользователя с несовпадающими паролями при подтверждении")
+    public void testRegistrationFailUserWithMismatchedPasswords() {
+        var request = getDefaultRegistration()
+                .setPassword("defaultPassword123")
+                .setConfirmPassword("defaultPassword12");
+
+        userSteps.registerUser(request)
+                .assertStatus(SC_BAD_REQUEST);
+    }
+
+    @Test(testName = "ТК-311- Негативные тесты на регистрацию", dataProvider ="InvalidFieldValues" )
+    public void registrationFailTests(String field, String value){
+       var request = getDefaultCreateTaskRequestMap();
+       request.put(field, value);
+
+       userSteps.registerUserMap(request)
+               .assertStatus(SC_BAD_REQUEST);
+    }
+
+    @DataProvider(name = "InvalidFieldValues")
+    public Object[][] dataProvider1() {
+        return new Object[][]{
+                {"email", ""},
+                {"email", " default@gmail.com "},
+                {"email", "default.gmail.com"},
+                {"email", "default@ru"},
+                {"email", "testEmail*mail.ru"}
+        };
     }
 }
