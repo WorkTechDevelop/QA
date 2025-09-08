2d5297f603e8f9704fdec9d32d72f20167a590de
ChaykinDenis <chaichkin.denis@gmail.com>
2025-09-08 09:58:13 +0300

    заменили в тесте хардкодные логин и пароль, теперь используем объект пользака

--- src/test/java/ru/worktech/registration_test/AuthorizationTests.java
+++ src/test/java/ru/worktech/registration_test/AuthorizationTests.java
@@ -1,29 +1,37 @@
 package ru.worktech.registration_test;
 
+import database.dto.UserDTO;
+import database.query.UserQueries;
+import database.utils.UserFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import ru.worktech.common.BaseApiTests;
+import ru.worktech.common.FreshUserTest;
 import ru.worktech.common.SharedUserTest;
 import ru.worktech.models.request.AuthorizationRequest;
+
+import java.sql.SQLException;
 
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.apache.http.HttpStatus.SC_UNAUTHORIZED;
 import static testDataGenerator.TestDataGenerator.generateRandomEmail;
 import static testDataGenerator.TestDataGenerator.generateRandomPassword;
 
-public class AuthorizationTests extends SharedUserTest {
+public class AuthorizationTests extends BaseApiTests {
 
-
-    private String password = "password12345";
+    private UserDTO currentUser;
 
     @Test(testName = "TK-311-1-Успешная авторизация")
-    public void testAuthorizationSuccess() {
-        userSteps.loginUser(new AuthorizationRequest("test3@mail.ru", "password12345"))
+    public void testAuthorizationSuccess() throws SQLException {
+        currentUser = UserFactory.createUser();
+        getUserSteps().loginUser(new AuthorizationRequest(currentUser.getEmail(), currentUser.getPassword()))
                 .assertStatus(SC_OK);
     }
 
+
     @Test(testName = "ТК-311-Тесты на авторизацию с невалидными данными", dataProvider = "dataProvider")
     public void testAuthorizationFail(String email, String password) {
-        userSteps.loginUser(new AuthorizationRequest(email, password))
+        getUserSteps().loginUser(new AuthorizationRequest(email, password))
                 .assertStatus(SC_UNAUTHORIZED);
     }
 
