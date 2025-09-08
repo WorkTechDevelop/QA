63e24753a3a2a3b5bbeb68161f77483e6ef93a93
ChaykinDenis <chaichkin.denis@gmail.com>
2025-09-08 09:59:12 +0300

    Вынесли все объекты которыми пользуются тесты, в отдельный "Базовый тестовый класс"

---
+++ src/test/java/ru/worktech/common/BaseApiTests.java
@@ -0,0 +1,43 @@
+package ru.worktech.common;
+
+import database.dto.UserDTO;
+import database.query.TaskQueries;
+import database.query.UserQueries;
+import ru.worktech.steps.TaskSteps;
+import ru.worktech.steps.UserSteps;
+
+public abstract class BaseApiTests {
+
+    private UserSteps userSteps;
+    private TaskSteps taskSteps;
+    private TaskQueries taskQueries;
+    private UserQueries userQueries;
+
+    protected UserSteps getUserSteps() {
+        if (userSteps == null) {
+            userSteps = new UserSteps();
+        }
+        return userSteps;
+    }
+
+    protected TaskSteps getTaskSteps() {
+        if (taskSteps == null) {
+            taskSteps = new TaskSteps();
+        }
+        return taskSteps;
+    }
+
+    protected TaskQueries getTaskQueries() {
+        if (taskQueries == null) {
+            taskQueries = new TaskQueries();
+        }
+        return taskQueries;
+    }
+
+    protected UserQueries getUserQueries() {
+        if (userQueries == null) {
+            userQueries = new UserQueries();
+        }
+        return userQueries;
+    }
+}
